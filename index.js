ef1356b8dfa0d3a9debf6c2180979b5cb4752a57
bernard-lin <bernardlin12@gmail.com>
2016-11-26 11:40:24 -0500

    reset 2

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
-var port = process.env.PORT || 8080
-
-
-
+var port = process.env.PORT || 8080;
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
